ad6dee97e9be1114c385907148e51e14cd9cd59c
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2025-02-17 21:15:05 +0900

    Product Card, Product List

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,9 +1,38 @@
+import type { MetaFunction } from "react-router";
+import { ProductCard } from "~/features/products/components/product-card";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Home | wemake" },
+    { name: "description", content: "Home to wemake" },
+  ];
+};
 
 export default function HomePage() {
   return (
-    <main className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold">환영합니다</h1>
-      <p className="mt-4">이곳은 홈페이지입니다.</p>
-    </main>
+    <div className="px-20">
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold loading-tight tracking-tight">
+            Today's Products
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The best products made by our community today.
+          </p>
+        </div>
+        <div>
+          {Array.from({ length: 10 }).map((_, index) => (
+            <ProductCard
+              id="productId"
+              name="Product Name"
+              description="Product Description"
+              commentCount={12}
+              viewCount={12}
+              upvoteCount={120}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
   );
-} 
+}
---
+++ app/features/products/components/product-card.tsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
+import { ChevronUpIcon, EyeIcon, MessageCircleIcon } from "lucide-react";
+import { Button } from "~/common/components/ui/button";
+
+interface ProductCardProps {
+  id: string;
+  name: string;
+  description: string;
+  commentCount: number;
+  viewCount: number;
+  upvoteCount: number;
+}
+
+export function ProductCard({
+  id,
+  name,
+  description,
+  commentCount,
+  viewCount,
+  upvoteCount,
+}: ProductCardProps) {
+  return (
+    <Link to={`/products/${id}`}>
+      <Card className="w-full flex items-center justify-between bg-transparent hover:bg-card/50">
+        <CardHeader>
+          <CardTitle className="text-2xl font-semibold leading-none tracking-tight">
+            {name}
+          </CardTitle>
+          <CardDescription className="text-muted-foreground">
+            {description}
+          </CardDescription>
+          <div className="flex items-center gap-4 mt-2">
+            <div className="flex items-center gap-px text-xs text-muted-foreground">
+              <MessageCircleIcon className="w-4 h-4" />
+              <span>{commentCount}</span>
+            </div>
+            <div className="flex items-center gap-px text-xs text-muted-foreground">
+              <EyeIcon className="w-4 h-4" />
+              <span>{viewCount}</span>
+            </div>
+          </div>
+        </CardHeader>
+        <CardFooter className="py-0">
+          <Button variant="outline" className="flex flex-col h-14">
+            <ChevronUpIcon className="size-4 shrink-0" />
+            <span>{upvoteCount}</span>
+          </Button>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+}

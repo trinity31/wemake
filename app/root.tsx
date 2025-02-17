8958270861fb718a0f04a4e04af0dfdb91877b61
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2025-02-17 12:56:36 +0900

    인증상태에 따라 오른쪽 메뉴 표시

--- app/root.tsx
+++ app/root.tsx
@@ -45,7 +45,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
     <>
-      <Navigation />
+      <Navigation isLoggedIn={true} hasNotifications={true} hasMessages={true} />
       <Outlet />
     </>
   );

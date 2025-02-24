23d1139b0c2bcd3c4ff848917dc36b1351edaf02
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2025-02-24 12:08:14 +0900

    Product Pagination

---
+++ app/common/components/product-pagination.tsx
@@ -0,0 +1,14 @@
+
+interface ProductPaginationProps {
+  totalPages: number;
+}
+
+export default function ProductPagination({
+  totalPages,
+}: ProductPaginationProps) {
+  return (
+    <div>
+      
+    </div>
+  )
+}

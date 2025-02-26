41175aeba05f14fd3c45850b6897ad795fe541ee
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2025-02-26 12:19:43 +0900

    Jobs, Profiles, Products 테이블

--- app/features/users/schema.ts
+++ app/features/users/schema.ts
@@ -1,1 +1,51 @@
-// models for users feature
+import {
+  jsonb,
+  pgEnum,
+  pgSchema,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+// 실제로 생성되지 않음 (users 테이블 이미 존재하므로), reference 를 하기 위해 정의하는 Fake table, 실제 테이블은 auth.users 테이블
+const users = pgSchema("auth").table("users", {
+  id: uuid().primaryKey(),
+});
+
+export const roles = pgEnum("role", [
+  "developer",
+  "designer",
+  "marketer",
+  "founder",
+  "product-manager",
+]);
+
+export const profiles = pgTable("profiles", {
+  profile_id: uuid()
+    .primaryKey()
+    .references(() => users.id, { onDelete: "cascade" }),
+  avatar: text(),
+  name: text().notNull(),
+  username: text().notNull(),
+  headline: text(),
+  bio: text(),
+  role: roles().default("developer").notNull(),
+  stats: jsonb().$type<{
+    followers: number;
+    following: number;
+  }>(),
+  views: jsonb(),
+  created_at: timestamp().notNull().defaultNow(),
+  updated_at: timestamp().notNull().defaultNow(),
+});
+
+export const follows = pgTable("follows", {
+  follower_id: uuid().references(() => profiles.profile_id, {
+    onDelete: "cascade",
+  }),
+  following_id: uuid().references(() => profiles.profile_id, {
+    onDelete: "cascade",
+  }),
+  created_at: timestamp().notNull().defaultNow(),
+});

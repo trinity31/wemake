de2c7998414910a7be9c68d749370775e92d6667
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2025-03-19 14:26:13 +0900

    Post replies with data fetching

--- app/features/community/queries.ts
+++ app/features/community/queries.ts
@@ -143,3 +143,29 @@ export const getPostById = async (postId: string) => {
   if (error) throw error;
   return data;
 };
+
+export const getReplies = async (postId: string) => {
+  const replyQuery = `
+    post_reply_id,
+    reply,
+    created_at,
+    user:profiles (
+      name,
+      avatar,
+      username
+    )
+  `;
+  const { data, error } = await client
+    .from("post_replies")
+    .select(
+      `
+      ${replyQuery},
+      post_replies (
+        ${replyQuery}
+      )
+      `
+    )
+    .eq("post_id", postId);
+  if (error) throw error;
+  return data;
+};

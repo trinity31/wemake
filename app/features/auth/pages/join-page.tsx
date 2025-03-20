95655b6272245e0ef5001ad83d69347101655ac2
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2025-03-20 17:27:02 +0900

    로그아웃, 이메일 회원가입

--- app/features/auth/pages/join-page.tsx
+++ app/features/auth/pages/join-page.tsx
@@ -1,14 +1,66 @@
 import { Button } from "~/common/components/ui/button";
-import { Form, Link } from "react-router";
+import { Form, Link, useNavigation } from "react-router";
 import { Route } from "./+types/join-page";
 import InputPair from "~/common/components/input-pair";
 import AuthButtons from "../components/auth-buttons";
+import { z } from "zod";
+import { makeSSRClient } from "~/supa-client";
+import { redirect } from "react-router";
+import { checkUsernameExists } from "../queries";
+import { LoaderCircle } from "lucide-react";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Join | wemake" }];
 };
 
-export default function JoinPage() {
+const formSchema = z.object({
+  name: z.string().min(3),
+  username: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export const action = async ({ request }: Route.ActionArgs) => {
+  const formData = await request.formData();
+  const { success, error, data } = formSchema.safeParse(
+    Object.fromEntries(formData)
+  );
+  if (!success) {
+    return {
+      formErrors: error.flatten().fieldErrors,
+    };
+  }
+  const usernameExists = await checkUsernameExists(request, {
+    username: data.username,
+  });
+  if (usernameExists) {
+    return {
+      formErrors: { username: ["Username already exists"] },
+    };
+  }
+  const { client, headers } = makeSSRClient(request);
+  const { error: signUpError } = await client.auth.signUp({
+    email: data.email,
+    password: data.password,
+    options: {
+      data: {
+        name: data.name,
+        username: data.username,
+      },
+    },
+  });
+  if (signUpError) {
+    return {
+      signUpError: signUpError.message,
+    };
+  }
+  return redirect("/", { headers });
+};
+
+export default function JoinPage({ actionData }: Route.ComponentProps) {
+  const navigation = useNavigation();
+  const isSubmitting =
+    navigation.state === "submitting" || navigation.state === "loading";
   return (
     <div className="flex flex-col relative items-center justify-center h-full">
       <Button variant={"ghost"} asChild className="absolute right-8 top-8 ">
@@ -16,7 +68,7 @@ export default function JoinPage() {
       </Button>
       <div className="flex items-center flex-col justify-center w-full max-w-md gap-10">
         <h1 className="text-2xl font-semibold">Create an account</h1>
-        <Form className="w-full space-y-4">
+        <Form className="w-full space-y-4" method="post">
           <InputPair
             label="Name"
             description="Enter your name"
@@ -26,6 +78,9 @@ export default function JoinPage() {
             type="text"
             placeholder="Enter your name"
           />
+          {actionData && "formErrors" in actionData && (
+            <p className="text-red-500">{actionData?.formErrors?.name}</p>
+          )}
           <InputPair
             id="username"
             label="Username"
@@ -35,6 +90,9 @@ export default function JoinPage() {
             type="text"
             placeholder="i.e wemake"
           />
+          {actionData && "formErrors" in actionData && (
+            <p className="text-red-500">{actionData?.formErrors?.username}</p>
+          )}
           <InputPair
             id="email"
             label="Email"
@@ -44,6 +102,9 @@ export default function JoinPage() {
             type="email"
             placeholder="i.e wemake@example.com"
           />
+          {actionData && "formErrors" in actionData && (
+            <p className="text-red-500">{actionData?.formErrors?.email}</p>
+          )}
           <InputPair
             id="password"
             label="Password"
@@ -53,9 +114,19 @@ export default function JoinPage() {
             type="password"
             placeholder="Enter your password"
           />
-          <Button className="w-full" type="submit">
-            Create account
+          {actionData && "formErrors" in actionData && (
+            <p className="text-red-500">{actionData?.formErrors?.password}</p>
+          )}
+          <Button className="w-full" type="submit" disabled={isSubmitting}>
+            {isSubmitting ? (
+              <LoaderCircle className="animate-spin" />
+            ) : (
+              "Create account"
+            )}
           </Button>
+          {actionData && "signUpError" in actionData && (
+            <p className="text-red-500">{actionData.signUpError}</p>
+          )}
         </Form>
         <AuthButtons />
       </div>
